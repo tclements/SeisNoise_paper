27f7baeeb0d4cccee40cb5f3bd69294003ca686e
timclements <timclements@pop-os.localdomain>
2020-03-02 17:17:14 -0500

    Update package adding for Julia 1.3

--- src/add-packages.jl
+++ src/add-packages.jl
@@ -1,5 +1,9 @@
 using Pkg
 
 # add following packages
-Pkg.add(["AWSCore", "AWSS3", "DataFrames", "CSV", "DSP", "FFTW", "Glob", "JLD2", "SeisIO",
-      "SeisNoise", "Interpolations", "GLM", "Plots"])
+pkgs = ["AWSCore", "AWSS3", "DataFrames", "CSV", "DSP", "FFTW", "Glob", "JLD2", "SeisIO",
+       "Interpolations", "GLM", "Plots"]
+for p in pkgs
+      Pkg.add(p)
+end
+Pkg.add(PackageSpec(name="SeisNoise",rev="GPU"))
