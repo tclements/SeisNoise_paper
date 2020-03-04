a67c8d629d2b71c0f6839131ce083d1d9c2440c9
Tim Clements <thclements@g.harvard.edu>
2020-03-04 10:09:02 -0500

    Add CuArrays and BenchmarkTools to package list

--- src/add-packages.jl
+++ src/add-packages.jl
@@ -1,8 +1,8 @@
 using Pkg
 
 # add following packages
-pkgs = ["AWSCore", "AWSS3", "DataFrames", "CSV", "DSP", "FFTW", "Glob", "JLD2", "SeisIO",
-       "Interpolations", "GLM", "Plots"]
+pkgs = ["AWSCore", "AWSS3", "DataFrames", "CSV", "Glob", "JLD2", "SeisIO",
+        "Plots, CuArrays", "BenchmarkTools"]
 for p in pkgs
       Pkg.add(p)
 end
