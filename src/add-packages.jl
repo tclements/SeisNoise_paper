d5f9fd6aeaab9b1c3f9ab8821e742a4ce0373914
Tim Clements <thclements@g.harvard.edu>
2020-03-05 08:07:32 -0500

    Add additional packages

--- src/add-packages.jl
+++ src/add-packages.jl
@@ -2,7 +2,7 @@ using Pkg
 
 # add following packages
 pkgs = ["AWSCore", "AWSS3", "DataFrames", "CSV", "Glob", "JLD2", "SeisIO",
-        "Plots", "CuArrays", "BenchmarkTools"]
+        "Plots", "CuArrays", "CUDAnative", "CUDAdrv", "BenchmarkTools"]
 for p in pkgs
       Pkg.add(p)
 end
