fae1788844dd31e18695f5e2f7be3e1797980368
Tim Clements <thclements@g.harvard.edu>
2020-05-15 17:23:57 -0400

    Change to autoinstall for Linux + MacOS

--- src/install_julia.sh
+++ src/install_julia.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-if [ -d "/shared" ]
-then
-    cd /shared
-else
-    cd
-fi
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.4/julia-1.4.1-linux-x86_64.tar.gz
-tar xvfa julia-1.4.1-linux-x86_64.tar.gz
-rm julia-1.4.1-linux-x86_64.tar.gz
-if [ -d "/shared" ]
-then
-    echo PATH=\$PATH:/shared/julia-1.4.1/bin/ >> ~/.bashrc
-else
-    echo PATH=\$PATH:~/julia-1.4.1/bin/ >> ~/.bashrc
-fi
+bash -ci "$(curl -fsSL https://raw.githubusercontent.com/abelsiqueira/jill/master/jill.sh)"
 source ~/.bashrc
 julia ~/SeisNoise_paper/src/add-packages.jl
-cd
